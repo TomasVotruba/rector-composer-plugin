7634932dc7873ef5a180ffb89b5114dab22f8ae0
Tomas Votruba <tomas.vot@gmail.com>
2022-01-24 13:04:37 +0100

    use array for paths

--- src/RectorConfigGenerator/ContentGenerator/PathsContentGenerator.php
+++ src/RectorConfigGenerator/ContentGenerator/PathsContentGenerator.php
@@ -5,8 +5,23 @@ declare(strict_types=1);
 namespace Rector\ComposerPlugin\RectorConfigGenerator\ContentGenerator;
 
 use Nette\Utils\Strings;
+use PhpParser\Node\Arg;
+use PhpParser\Node\Expr;
+use PhpParser\Node\Expr\Array_;
+use PhpParser\Node\Expr\ArrayItem;
+use PhpParser\Node\Expr\BinaryOp\Concat;
+use PhpParser\Node\Expr\ClassConstFetch;
+use PhpParser\Node\Expr\MethodCall;
+use PhpParser\Node\Expr\Variable;
+use PhpParser\Node\Identifier;
+use PhpParser\Node\Name;
+use PhpParser\Node\Scalar\MagicConst\Dir;
+use PhpParser\Node\Scalar\String_;
+use PhpParser\Node\Stmt;
+use PhpParser\Node\Stmt\Expression;
 use Rector\ComposerPlugin\Contract\ConfigContentGeneratorInterface;
 use Rector\ComposerPlugin\FileSystem\PathNormalizer;
+use Rector\ComposerPlugin\PhpParser\Printer\IndentAwarePrinter;
 use Symfony\Component\Finder\Finder;
 use Symfony\Component\Finder\SplFileInfo;
 use Webmozart\Assert\Assert;
@@ -26,9 +41,15 @@ final class PathsContentGenerator implements ConfigContentGeneratorInterface
      */
     private $pathNormalizer;
 
+    /**
+     * @var IndentAwarePrinter
+     */
+    private $indentAwarePrinter;
+
     public function __construct()
     {
         $this->pathNormalizer = new PathNormalizer();
+        $this->indentAwarePrinter = new IndentAwarePrinter();
     }
 
     /**
@@ -38,16 +59,23 @@ final class PathsContentGenerator implements ConfigContentGeneratorInterface
     {
         Assert::allString($paths);
 
-        $pathsContent = '';
-        foreach ($paths as $path) {
-            $path = $this->pathNormalizer->normalize($path);
+        $stmts = [];
 
-            // @todo use node printer
-            $pathsContent .= '        __DIR__ . \'/' . $path . "'," . PHP_EOL;
+        $pathArrayItems = [];
+        foreach ($paths as $path) {
+            $path = '/' . $this->pathNormalizer->normalize($path);
+
+            $concat = new Concat(new Dir(), new String_($path));
+            $pathArrayItems[] = new ArrayItem($concat);
         }
 
-        // remove extra new line on the right
-        return rtrim($pathsContent);
+        $pathsArray = new Array_($pathArrayItems, [
+            'kind' => Array_::KIND_SHORT,
+        ]);
+
+        $stmts[] = $this->createSetParameterMethodCall('PATHS', $pathsArray);
+
+        return $this->indentAwarePrinter->prettyPrintWithIndent($stmts);
     }
 
     /**
@@ -91,5 +119,17 @@ final class PathsContentGenerator implements ConfigContentGeneratorInterface
         }
 
         return $relativeDirectoryPaths;
+    }
+
+    private function createSetParameterMethodCall(string $parameterName, Expr $expr): Stmt
+    {
+        $args = [
+            new Arg(new ClassConstFetch(new Name('Option'), new Identifier($parameterName))),
+            new Arg($expr),
+        ];
+
+        $parametersSetMethodCall = new MethodCall(new Variable('parameters'), new Identifier('set'), $args);
+
+        return new Expression($parametersSetMethodCall);
     }
 }
